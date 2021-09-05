1f3f122ae35bea4af6ef95afc7fe22852d8dbdd2
Peggy <peggymailula@gmail.com>
2021-09-05 16:07:06 +0200

    corrections

--- reg-factory.js
+++ reg-factory.js
@@ -1,7 +1,5 @@
 function registrationFactoryFunction() {
-
   var capeCities = [];
-
   var towns = {
     
     'CY' : 'Bellville',
@@ -9,34 +7,27 @@ function registrationFactoryFunction() {
     'CA' : 'Cape Town',
     
   }
-
  
   var validRegistrations;
   var invalidRegistrations;
   var duplicates = [];
   var invalidRegNums = [];
-
   function regList(inputReg) {
     userReg.unshift(inputReg);
   }
-
   function getTown(town){
     if(town.startsWith("CA") || town.startsWith("CL") || town.startsWith("CY")){
         capeCities.push(town);
        }
        return capeCities;
   }
-
   function registrations(inputReg) {
-
       var str;
       var townFiltered = [];
-
         for (var town in towns) {
           if (towns[town] === inputReg) {
             str = town;
           }
-
       }
      
     userReg.forEach(function(car) {
@@ -44,13 +35,10 @@ function registrationFactoryFunction() {
          townFiltered.push(car);
          }
        
-
     });
-
     return townFiltered;
     
   }
-
   function returnErrors(town){
       if(/^((CY|CL|CA|CAA)\s\d([0-9]){4})$/.test(reg) && /^((CJ|CY|CL|CA)\-([0-9]){6})$/.test(reg) && /^((CJ|CY|CL|CA)\-([0-9]){5})$/.test(reg)) {
             if(reg === town){
@@ -64,8 +52,6 @@ function registrationFactoryFunction() {
         return correctFormat;   
       }
   }
-
-
   function checkRegNumbers(reg) {
   
     if(/^((CJ|CY|CL|CA)\-([0-9]){3}\-([0-9]){3})$/.test(reg) || /^((CJ|CY|CL|CA)\-([0-9]){6})$/.test(reg) || /^((CJ|CY|CL|CA)\-([0-9]){5})$/.test(reg)) {
@@ -87,10 +73,7 @@ function registrationFactoryFunction() {
     }
     return true;
   }
-
-
   function caseFormat(str) {
-
     str = str.toUpperCase();
   
     var list = str.split(',');
@@ -100,10 +83,6 @@ function registrationFactoryFunction() {
     });
     return list;
   }
-
-
-
-
   return {
       regList,
       checkRegNumbers,
